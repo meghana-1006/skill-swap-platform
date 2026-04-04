e9bf620d89c66d91d6c5e18100dcbdd0471e5a48
Meghana <meghanagandluri02@gmail.com>
2026-04-04 16:48:14 +0530

    Fixed API route prefix

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://skill-swap-platform-1-tl4r.onrender.com",
+  baseURL: "https://skill-swap-platform-1-tl4r.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
